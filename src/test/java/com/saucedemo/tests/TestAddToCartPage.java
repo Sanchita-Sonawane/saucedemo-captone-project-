7277ee53e4421f8cbef58c955d50495459c441b0
Sanchita-Sonawane <142747190+Sanchita-Sonawane@users.noreply.github.com>
2025-09-08 10:16:19 +0530

    Update TestAddToCartPage.java

--- src/test/java/com/saucedemo/tests/TestAddToCartPage.java
+++ src/test/java/com/saucedemo/tests/TestAddToCartPage.java
@@ -35,9 +35,8 @@ public class TestAddToCartPage extends BaseTest {
     @Test
     public void cartBadgeAccumulatesItems() {
         AddToCartPage atc = new AddToCartPage(driver);
-        atc.addItem(); // 1st item
-        // Simulate adding another item (if more items are available, add them)
-        driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click(); // Example second item
+        atc.addItem(); 
+        driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
         HomePage hp = new HomePage(driver);
         Assert.assertEquals(hp.getCartBadgeCount(), "2", "Cart badge should show 2 after adding two items.");
     }
@@ -50,39 +49,22 @@ public class TestAddToCartPage extends BaseTest {
         String expectedUrl = "https://www.saucedemo.com/cart.html";
         Assert.assertEquals(driver.getCurrentUrl(), expectedUrl, "User should be navigated to the cart page.");
     }
-
-    /*@Test
-    public void addingSameItemDoesNotDuplicateInBadge() {
-        AddToCartPage atc = new AddToCartPage(driver);
-        atc.addItem();
-        // Try to add same item again (shouldn't increment cart badge further)
-        // You may skip this if the site disables the button after one click.
-        atc.addItem(); // no effect expected
-        HomePage hp = new HomePage(driver);
-        Assert.assertEquals(hp.getCartBadgeCount(), "1", "Cart badge should not increment when same item is added again.");
-    }*/
-
-    @Test
-    public void cartBadgeNotVisibleWhenEmpty() {
-        HomePage hp = new HomePage(driver);
-        Assert.assertEquals(hp.getCartBadgeCount(), "0", "Cart badge should be 0 or not visible when no items added.");
-    }
     
-    /*@Test
+    @Test
     public void addToCartButtonChangesToRemove() {
         AddToCartPage atc = new AddToCartPage(driver);
         atc.addItem();
         String buttonText = atc.getAddRemoveButtonText();
         Assert.assertEquals(buttonText, "Remove", "Button should show 'Remove' after adding item to cart.");
-    }*/
+    }
 
-    /*@Test
+    @Test
     public void removeFromCartButtonChangesToAddToCart() {
         AddToCartPage atc = new AddToCartPage(driver);
         atc.addItem();
         atc.removeItem();
         String buttonText = atc.getAddRemoveButtonText();
         Assert.assertEquals(buttonText, "Add to cart", "Button should show 'Add to cart' after removing item from cart.");
-    }*/
+    }
 
 }
